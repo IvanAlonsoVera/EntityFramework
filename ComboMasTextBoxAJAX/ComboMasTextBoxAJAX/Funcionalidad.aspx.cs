77c49f12752784c7b68e00faeed06661c045641d
Iván Alonso Vera <alonso.vera.ivan@gmail.com>
2023-02-01 20:53:05 +0100

    010223

---
+++ ComboMasTextBoxAJAX/ComboMasTextBoxAJAX/Funcionalidad.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComboMasTextBoxAJAX
+{
+    public partial class Funcionalidad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        [System.Web.Services.WebMethod]
+        public static List<SupplierCargar> CargarDatos()
+        {
+            using (var context = new NORTHWNDEntities())
+            {
+                var query = context.Suppliers
+                    .Select(s => new SupplierCargar
+                    {
+                        SupplierID = s.SupplierID,
+                        ContactName = s.ContactName,
+                    }).ToList();
+                return query;
+            }
+        }
+        [System.Web.Services.WebMethod]
+        public static Datos RellenarTextBox(string otracosa)
+        {
+            int idDelSupplier = Convert.ToInt32(otracosa);
+            using (var context = new NORTHWNDEntities())
+            {
+                var query = (context.Products
+                    .Where(p => p.SupplierID == idDelSupplier)
+                    .OrderByDescending(p=>p.UnitsOnOrder)
+                    .Select(p => new Datos
+                    {
+                        Nombre = p.ProductName,
+                        Maximo = (short)p.UnitsOnOrder
+                    })).FirstOrDefault();
+                return query;
+            }
+        }
+    }
+}
---
+++ EjercicioCoche(Basico)/EjercicioCoche(Basico)/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using EjercicioCoche_Basico_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjercicioCoche_Basico_.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "sin datos";
+        }
+        public ViewResult CrearCoche()
+        {
+            Coche coche1 = new Coche()
+            {
+                Color="Blanco",
+                Modelo="Transit",
+                Marca="Ford"
+            };
+
+            return View("Index",(object)string.Format("Marca: {0} Modelo: {1} Color: {2}",coche1.Marca,coche1.Modelo,coche1.Color));
+        }
+        public ViewResult CrearCliente()
+        {
+
+            string[] clientes = new string[]{ "pepe", "juan" };
+
+            return View("Index", (object)string.Format("Nombre Cliente {0}", clientes[0]));
+        }
+    }
+}
